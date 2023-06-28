d17810b4b5bbddcc44349b84c5f31138f8b3c89d
Yuze Wang <yuzewang621@gmail.com>
2023-06-28 09:59:56 -0600

    add routes for about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  # get '/about', to: 'about#index'
+  resources :about, only: [:index]
+
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
 
   resource :cart, only: [:show] do
     post   :add_item
