45099ed5fc2364245f7cb382804cbdf28ba641cd
Yuze Wang <yuzewang621@gmail.com>
2023-07-02 12:38:44 -0600

    add cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,18 @@
+describe('jungle home page', () => {
+  // beforeEach(() => {
+  //   cy.visit('/')
+  // })
+
+  it('should visit the home page', () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+
+})
