3e49acc175f1a53efc6eb109e09bee118625abd4
Yuze Wang <yuzewang621@gmail.com>
2023-07-02 14:10:54 -0600

    add cypress for product details page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,28 @@
+describe('jungle home page', () => {
+  // beforeEach(() => {
+  //   cy.visit('/')
+  // })
+
+  it('should visit the home page', () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it('should navigate from home page to product detail page', () => {
+    cy.get('.products').children().first().click(); 
+
+    // Assert that the product detail page is loaded
+    cy.url().should('include', '/products/');
+  
+    cy.contains('Add');
+
+  });
+
+
+})
