6e27915857e1f8aa75e3c288a12fc4d1f6d3da24
Yuze Wang <yuzewang621@gmail.com>
2023-07-03 06:28:06 -0600

    change soldout badge function to helper

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,2 +1,12 @@
 module ProductHelper
+
+  def sold_out_button(product)
+    if product.sold_out?
+      content_tag(:button, 'Sold Out', class: 'soldout')
+    end
+  end
+
+  def add_button_disabled(product)
+    'disabled' if product.sold_out?
+  end
 end
